37a2739f647d31b22038e2a38b6826c1548cf055
Ric Valenzuela <ricvalenzuela17@gmail.com>
2025-08-08 16:40:26 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 $conn = mysqli_connect("localhost", "root", "", "itlab");
 
-if($conn){
+if(!$conn){
     die("connection failed");
 }
 
@@ -9,14 +9,15 @@ $username = $_POST['id'];
 $password = $_POST['password'];
 
 $sql = "SELECT * FROM admin WHERE id= '$id'  AND password ='$password'";
-$result = mysql_query($conn, $sql);
+$result = mysqli_query($conn, $sql);
 
-if (mysql_num_rows($result) > 0){
+if (mysqli_num_rows($result) > 0){
     echo"success = )";
     exit();
 }else{
     echo"Login failed!";
 }
 
-mysql_close($conn);
+mysqli_close($conn);
+
 ?>
